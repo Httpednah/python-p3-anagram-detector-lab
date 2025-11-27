7a75d37a4e379382406f8633c5f6006dfb8e1566
Ednah wairimu kimani <kimaniednah@gmail.com>
2025-11-27 19:44:39 +0300

    lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        # store the word in lowercase to make comparison case-insensitive
+        self.word = word.lower()
+
+    def match(self, words):
+        # return a list of words from the input that are anagrams of self.word
+        matches = []
+        sorted_word = sorted(self.word)
+        for w in words:
+            if sorted(w.lower()) == sorted_word:
+                matches.append(w)
+        return matches
